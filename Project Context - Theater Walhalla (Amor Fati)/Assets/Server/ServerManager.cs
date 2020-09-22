c2a5c080ade6afa5b4560ddd2577afa9c188bea9
DESKTOP-OIQUDUT\marci <marcianosordam@gmail.com>
2020-09-22 15:41:43 +0200

    Server Setup. Folder "Server" toegevoegt in het mapje "Assets";

---
+++ Project Context - Theater Walhalla (Amor Fati)/Assets/Server/ServerManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+
+public class ServerManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("pressed");
+            StartCoroutine(GetRequest());
+        }
+    }
+
+    IEnumerator GetRequest()
+    {
+        string uri = "https://www.slatecanvas.com/context3";
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+            }
+        }
+    }
+}
