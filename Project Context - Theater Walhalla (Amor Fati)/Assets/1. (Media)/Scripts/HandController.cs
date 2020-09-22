508b462c387840c38d0a9f2f928331e9278fd804
Markreel <markmeerssman@live.nl>
2020-09-22 15:08:31 +0200

    Started working on folder structure Started working on the media minigame prototype

---
+++ Project Context - Theater Walhalla (Amor Fati)/Assets/1. (Media)/Scripts/HandController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector3 temp = Input.mousePosition;
+        temp.z = 3f; // Set this to be the distance you want the object to be placed in front of the camera.
+        this.transform.position = Camera.main.ScreenToWorldPoint(temp);    
+    }
+}
---
+++ Project Context - Theater Walhalla (Amor Fati)/Assets/1. (Media)/Scripts/MediaProjectile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MediaProjectile : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.Translate(transform.forward);
+    }
+}
