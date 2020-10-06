7387c1c1837df5301ca408623b4292612feeafaf
LorenzoHKU <lorenzohartman2000@gmail.com>
2020-10-06 17:28:28 +0200

    Added my own Folder. includes a Scene in which Televisions turn on if they are triggered from the TV_Director Timeline.

---
+++ Project Context - Theater Walhalla (Amor Fati)/Assets/3. (Lorenzo)/Scripts/TVManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TVManager : MonoBehaviour
+{
+    public List<VideoPlayer> videoPlayers;
+    private int currentTV; 
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponentsInChildren(true, videoPlayers);
+
+    }
+
+
+    public void TurnOn()
+    {
+        videoPlayers[currentTV].Play();
+        currentTV++;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
