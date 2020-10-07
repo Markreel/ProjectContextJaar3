8f116a29aa3bfb213a2100cf8549f0f24c07e1a4
LorenzoHKU <lorenzohartman2000@gmail.com>
2020-10-07 17:40:10 +0200

    Added Leantween and major work on the De Traditie - TV Concept Scene.

--- Project Context - Theater Walhalla (Amor Fati)/Assets/3. (Lorenzo)/Scripts/TVManager.cs
+++ Project Context - Theater Walhalla (Amor Fati)/Assets/3. (Lorenzo)/Scripts/TVManager.cs
@@ -15,13 +15,30 @@ public class TVManager : MonoBehaviour
     {
         GetComponentsInChildren(true, videoPlayers);
 
+        //The first item in the list is the static video player, we do not want to turn the static on but the next videoplayer instead.
+        currentTV = 1;
+
+
     }
 
 
     public void TurnOn()
     {
         videoPlayers[currentTV].Play();
-        currentTV++;
+        //We need to add two because the Static video also counts as a video player.
+        //This results in the hierachy being static - video - static - video.
+        //Fix this later.
+        currentTV += 2;
+        Debug.Log(currentTV);
+
+        //if (videoPlayers[currentTV].CompareTag("Video") == true)
+        //{
+        //    videoPlayers[currentTV].Play();
+        //    currentTV ++;
+        //}
+
+
+
 
     }
 
---
+++ Project Context - Theater Walhalla (Amor Fati)/Assets/3. (Lorenzo)/Scripts/TV_Instructions.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TV_Instructions : MonoBehaviour
+{
+    public GameObject title;
+    public GameObject tutorial;
+
+    [Header ("Title")]
+    public Vector3 introStartingSize;
+    public Vector3 introEndSize;
+    public float introFadetime;
+
+    [Header ("Tutorial")]
+    public Vector3 tutorialStartingSize;
+    public Vector3 tutorialEndSize;
+    public float tutorialFadetime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        title.transform.localScale = introStartingSize;
+        tutorial.transform.localScale = tutorialStartingSize;
+    }
+
+    public void FadeInTitle()
+    {
+        LeanTween.scale(title, introEndSize, introFadetime).setEaseInCubic();
+    }
+
+    public void FadeOutTitle()
+    {
+        LeanTween.scale(title, introStartingSize, introFadetime).setEaseOutCubic();
+    }
+
+    public void FadeInInstructions()
+    {
+        LeanTween.scale(tutorial, tutorialEndSize, tutorialFadetime).setEaseInCubic();
+    }
+
+    public void FadeOutInstructions()
+    {
+        LeanTween.scale(tutorial, tutorialStartingSize, tutorialFadetime).setEaseOutCubic();
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
