5f55b97dba746c90e79bfa4146599caffa7c2449
Marciano <marcianosordam@gmail.com>
2020-10-08 12:54:58 +0200

    matching minigame prototype, geen lerp

---
+++ Project Context - Theater Walhalla (Amor Fati)/Assets/1. (Media)/Scenes/marciano_matching/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] string selected;
+    [SerializeField] GameObject[] objects;
+
+    [SerializeField] GameObject[] selectedobjects;
+
+
+    void Update()
+    {
+
+        if ( Input.GetMouseButtonDown (0)){ 
+            RaycastHit hit; 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+            if ( Physics.Raycast (ray,out hit,100.0f)) {
+                Debug.Log("You selected " + hit.transform.name);
+            }
+
+            if (selected == ""){
+                selected = hit.transform.name;
+                hit.collider.SendMessage("Selected");
+                selectedobjects[0] = hit.collider.gameObject;
+            } 
+            else if(selected == hit.transform.name){
+                hit.collider.SendMessage("Selected");
+                selectedobjects[1] = hit.collider.gameObject;
+                Debug.Log("goodshot");
+                selected = "";
+            } 
+            else {
+                selectedobjects[1] = hit.collider.gameObject;
+                
+                selectedobjects[0].SendMessage("Deselect");
+                selectedobjects[1].SendMessage("Deselect");
+
+                Debug.Log("wrong");
+                selected = "";
+            }
+        }
+
+    }
+    
+}
---
+++ Project Context - Theater Walhalla (Amor Fati)/Assets/1. (Media)/Scenes/marciano_matching/ObjectScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScript : MonoBehaviour
+{
+    [SerializeField] Vector3 target;
+    [SerializeField] Vector3 origin;
+    [SerializeField] Vector3 offset;
+
+    void Start(){
+        origin = transform.position;
+        target = transform.position + offset;
+    }
+
+    public void Selected(){
+                Debug.Log(gameObject.name + " selected");
+
+        transform.position = target;
+
+    }
+
+    public void Deselect(){
+        Debug.Log(gameObject.name + " deselected");
+        transform.position = origin;
+    }
+}
