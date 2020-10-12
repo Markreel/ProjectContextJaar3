13c42a8ab4d7580df1eeda95b23221a3e0333ce1
Marciano <marcianosordam@gmail.com>
2020-10-12 16:46:04 +0200

    lights can be visualized

---
+++ Project Context - Theater Walhalla (Amor Fati)/Assets/VisualizeLight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualizeLight : MonoBehaviour
+{
+    [SerializeField] AudioVisualizerScript audiovisual;
+    [SerializeField] float target;
+    [SerializeField] float multiplyer;
+    [SerializeField] float minimumSize;
+    [SerializeField] Light locallight;
+
+    [SerializeField] float maxSize;
+    void Start()
+    {
+        locallight = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        target = Mathf.Clamp((audiovisual.spectrum[0] * multiplyer + minimumSize),0,maxSize);
+
+        locallight.intensity = target;
+        
+    }
+}
