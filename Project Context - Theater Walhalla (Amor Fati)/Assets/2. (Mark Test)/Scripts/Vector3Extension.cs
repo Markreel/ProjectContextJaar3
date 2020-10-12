c148855dce8e263d97ad3bfd409d989b5a22b7a2
Markreel <markmeerssman@live.nl>
2020-10-12 21:50:39 +0200

    Started working on Fati visuals

---
+++ Project Context - Theater Walhalla (Amor Fati)/Assets/2. (Mark Test)/Scripts/Vector3Extension.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Vector3Extension
+{
+    public static Vector3 Vector3Random(Vector3 _a, Vector3 _b)
+    {
+        return new Vector3(Random.Range(_a.x, _b.x), Random.Range(_a.y, _b.y), Random.Range(_a.z, _b.z));
+    }
+}
