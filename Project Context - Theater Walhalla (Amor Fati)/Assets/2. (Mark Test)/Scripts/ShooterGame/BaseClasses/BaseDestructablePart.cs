a5c6e27393c42a3f560606c79d071a31e41ca572
Markreel <markmeerssman@live.nl>
2020-11-04 17:10:14 +0100

    Created an example video for the gold pile Started working on a prototype for the Shooter Game

---
+++ Project Context - Theater Walhalla (Amor Fati)/Assets/2. (Mark Test)/Scripts/ShooterGame/BaseClasses/BaseDestructablePart.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace ShooterGame
+{
+    [RequireComponent(typeof(Rigidbody), typeof(SelfDestruct))]
+    public class BaseDestructablePart : MonoBehaviour, IDestructablePart
+    {
+        [SerializeField] private List<BaseDestructablePart> childParts = new List<BaseDestructablePart>();
+        [SerializeField] private UnityEvent onDestruction;
+
+        private Rigidbody rb;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        public void Destruct(IDestructable _parent)
+        {
+            foreach (var _part in childParts)
+            {
+                _parent.DestroyPart(_part);
+            }
+
+            rb.isKinematic = false;
+            transform.parent = transform.parent.parent;
+            rb.AddForce((Random.insideUnitSphere + Vector3.up) * 200f);
+            transform.LookAt(transform.position + Random.insideUnitSphere);
+            onDestruction.Invoke();
+        }
+    }
+}
