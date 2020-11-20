ecda88a8487570fd9116e7b0b7836da89a3ce894
Marciano <marcianosordam@gmail.com>
2020-11-20 13:34:45 +0100

    Fati final & Video Online Connection check

--- Project Context - Theater Walhalla (Amor Fati)/Assets/2. (Marciano test)/scripts/VisualizeScale.cs
+++ Project Context - Theater Walhalla (Amor Fati)/Assets/2. (Marciano test)/scripts/VisualizeScale.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class VisualizeScale : MonoBehaviour
 {
@@ -8,11 +9,46 @@ public class VisualizeScale : MonoBehaviour
     [SerializeField] float minimumSize;
     [SerializeField] float maxSize;
     [SerializeField] float speed;
+    [SerializeField] int offset = 0;
+    Vector3 scaleOffset;
+    float morphSpeed = 2f;
+    Vector3 sinX;
+    Vector3 sinOffset;
+    [SerializeField] float minScaleSize;
+
+    void Start()
+    {
+        audiovisual = GameObject.Find("AudioVisualizer").GetComponent<AudioVisualizerScript>();
+        sinX = new Vector3(Random.Range(0, 5), Random.Range(0, 5), Random.Range(0, 5));
+    }
+
     void Update()
     {
-        target = Mathf.Lerp(target, Mathf.Clamp((audiovisual.spectrum[0] * multiplyer + minimumSize),0,maxSize), speed*Time.deltaTime);
+        //sin wave offset X
+        sinX.x += 1 * Time.deltaTime * morphSpeed ;
+        scaleOffset.x = ((0.5f * Mathf.Sin(sinX.x))+ .75f);
 
-        transform.localScale = new Vector3(target,target,target);
+        //sin wave offset Y
+        sinX.y += 1 * Time.deltaTime * morphSpeed ;
+        scaleOffset.y = ((0.5f * Mathf.Sin(sinX.y))+ .75f);
+
+        //sin wave offset Z
+        sinX.z += 1 * Time.deltaTime * morphSpeed;
+        scaleOffset.z = ((0.5f * Mathf.Sin(sinX.z))+ .75f);
+
+        //Debug.Log(scaleOffset);
+
+
+
+        target = Mathf.Lerp(target, Mathf.Clamp((audiovisual.spectrum[offset] * multiplyer + minimumSize),0,maxSize), speed*Time.deltaTime);
+
+        scaleOffset.x = Mathf.Round(scaleOffset.x * 10f) / 10f;
+        scaleOffset.y = Mathf.Round(scaleOffset.y * 10f) / 10f;
+        scaleOffset.z = Mathf.Round(scaleOffset.z * 10f) / 10f;
+
         
+
+        transform.localScale = new Vector3(Mathf.Clamp((target* scaleOffset.x), minScaleSize, maxSize), Mathf.Clamp((target * scaleOffset.y), minScaleSize, maxSize), Mathf.Clamp((target * scaleOffset.z), minScaleSize, maxSize));      
     }
+
 }
---
+++ Project Context - Theater Walhalla (Amor Fati)/Assets/VideoEndDetectionTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class VideoEndDetectionTest : MonoBehaviour
+{
+    public VideoPlayer vp;
+    [SerializeField] GameObject endScreen;
+
+    private void Update()
+    {
+        if (!vp.isPlaying && vp.time > vp.length/10)
+        {
+            Debug.Log("I AM DONE");
+            endScreen.SetActive(true);
+        }
+    }
+}
