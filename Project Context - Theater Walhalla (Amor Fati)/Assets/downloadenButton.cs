ce3acefe0960df522a1002d13268def7bff2a0db
Blanke,M.E. (Manon) <m.e.blanke@students.uu.nl>
2020-12-14 11:10:29 +0100

    Update muziekgame art

--- Project Context - Theater Walhalla (Amor Fati)/Assets/downloadenButton.cs
+++ Project Context - Theater Walhalla (Amor Fati)/Assets/downloadenButton.cs
@@ -9,6 +9,11 @@ public class downloadenButton : MonoBehaviour, IPointerEnterHandler, IPointerExi
     [SerializeField] Sprite idle;
     [SerializeField] Sprite active;
 
+    public void OnEnable()
+    {
+        this.GetComponent<Image>().sprite = idle;
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         this.GetComponent<Image>().sprite = active;
--- Project Context - Theater Walhalla (Amor Fati)/Assets/menuButton.cs
+++ Project Context - Theater Walhalla (Amor Fati)/Assets/menuButton.cs
@@ -9,6 +9,11 @@ public class menuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     [SerializeField] Sprite idle;
     [SerializeField] Sprite active;
 
+    public void OnEnable()
+    {
+        this.GetComponent<Image>().sprite = idle;
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         this.GetComponent<Image>().sprite = active;
